059eeda4938afdb22e3b3ff915f96e341d1be9ba
Denis Kudelin <v-dekude@microsoft.com>
2016-08-29 16:29:29 +0300

    Added test for ChordChart

---
+++ src/CustomVisualsTests/visuals/ChordChart/chordChartTest.ts
@@ -0,0 +1,36 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("ChordChart", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("svg.chordChart g.slices > *")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.ChordChart();
+        clientHelpers.clickElement(visual.slices.eq(0));
+
+        visual.slices.toArray().map($).forEach((e,i) => {
+            if(i >= 1) {
+                expect(parseFloat(e.css('opacity'))).toBeLessThan(1);
+            } else {
+                expect(parseFloat(e.css('opacity'))).toBe(1);
+            }
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("5");
+            done();
+        }, 500);
+    });
+}));
---
+++ src/CustomVisualsTests/visuals/ChordChart/helpers.ts
@@ -0,0 +1,43 @@
+﻿namespace clientVisuals {
+	export class ChordChart {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.chordChart").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement() {
+            return this.rootElement
+                .children("svg.chordChart")
+                .children("g");
+        }
+
+        public get dataLabels() {
+            return this.mainElement
+                .children("g.labels")
+                .children("text.data-labels");
+        }
+
+        public get sliceTicks() {
+            return this.mainElement
+                .children("g.ticks")
+                .children("g.slice-ticks");
+        }
+
+        public get chords() {
+            return this.mainElement
+                .children("g.chords")
+                .children("path.chord");
+        }
+
+        public get slices() {
+            return this.mainElement
+                .children("g.slices")
+                .children("path.slice");
+        }
+	}
+}
