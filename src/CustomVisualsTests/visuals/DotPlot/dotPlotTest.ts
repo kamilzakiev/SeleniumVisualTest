b7e7d46f7fc9d721196638422cd4472016e94f07
Denis Kudelin <v-dekude@microsoft.com>
2016-08-29 16:37:33 +0300

    Added test for DotPlot

--- src/CustomVisualsTests/visuals/DotPlot/dotPlotTest.ts
+++ src/CustomVisualsTests/visuals/DotPlot/dotPlotTest.ts
@@ -1,6 +1,6 @@
 import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
 
-describe("AsterPlot", config.getSpecs(__dirname, (browser, reportUrl) => {
+describe("DotPlot", config.getSpecs(__dirname, (browser, reportUrl) => {
     let client: WebdriverIO.Client<void>;
     let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
     });
@@ -10,18 +10,18 @@ describe("AsterPlot", config.getSpecs(__dirname, (browser, reportUrl) => {
         client = webdriverIOHelpers.getWebClient(browser);
         client
             .url(reportUrl)
-            .waitForVisible("svg.asterPlot g.asterSlices")
+            .waitForVisible("svg.dotplot g.dotplotGroup > *")
             .then(() => done());
     });
 
     afterEach((done) => client.endAll().finally(() => done()));
 
     itClient("selection test", function (done) {
-        var visual = new clientVisuals.AsterPlot();
-        clientHelpers.clickElement(visual.slices.eq(0));
-        clientHelpers.clickElement(visual.slices.eq(1), true);
+        var visual = new clientVisuals.DotPlot();
+        clientHelpers.clickElement(visual.dotGroups.eq(0));
+        clientHelpers.clickElement(visual.dotGroups.eq(1), true);
 
-        visual.slices.toArray().map($).forEach((e,i) => {
+        visual.dotGroups.toArray().map($).forEach((e,i) => {
             if(i >= 2) {
                 expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
             } else {
@@ -30,7 +30,7 @@ describe("AsterPlot", config.getSpecs(__dirname, (browser, reportUrl) => {
         });
 
         setTimeout(() => {
-            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("2.47K");
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("$152K");
             done();
         }, 500);
     });
---
+++ src/CustomVisualsTests/visuals/DotPlot/helpers.ts
@@ -0,0 +1,45 @@
+﻿namespace clientVisuals {
+	export class DotPlot {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.dotplot").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement() {
+            return this.rootElement.find("svg.dotplot");
+        }
+
+        public get dataLabels() {
+            return this.mainElement.children("g.labels").children("text.data-labels");
+        }
+
+        public get axisGraphicsContext() {
+            return this.mainElement.children("g.axisGraphicsContext");
+        }
+
+        public get xAxis() {
+            return this.axisGraphicsContext.children("g.x.axis");
+        }
+
+        public get xAxisLabel() {
+            return this.xAxis.children("text.xAxisLabel");
+        }
+
+        public get dotGroups() {
+            return this.mainElement.children("g.dotplotSelector").children("g.dotplotGroup");
+        }
+
+        public get dots() {
+            return this.dotGroups.children("circle.circleSelector");
+        }
+
+        public get xAxisTicks() {
+            return this.xAxis.children("g.tick");
+        }
+	}
+}
