afc292af711cf2c14a89959e12c6653ff38f7a26
Denis Kudelin <v-dekude@microsoft.com>
2016-08-29 21:29:36 +0300

    Added test for Timeline

---
+++ src/CustomVisualsTests/visuals/Timeline/helpers.ts
@@ -0,0 +1,47 @@
+﻿namespace clientVisuals {
+	export class Timeline {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.timeline").parent().parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement(): JQuery {
+            return this.rootElement
+                .children("div")
+                .children("svg.timeline");
+        }
+
+        public get cellRects(): JQuery {
+            return this.mainArea
+                .children(".cellsArea")
+                .children(".cellRect");
+        }
+
+        public get mainArea() {
+            return this.mainElement
+                .children("g.mainArea");
+        }
+
+        public get allLabels() {
+            return this.mainArea
+                .children("g")
+                .children("text.label");
+        }
+
+        public get rangeHeaderText() {
+            return this.mainElement
+                .children("g.rangeTextArea")
+                .children("text.selectionRangeContainer");
+        }
+
+        public get timelineSlicer() {
+            return this.mainElement
+                .children("g.timelineSlicer");
+        }
+	}
+}
---
+++ src/CustomVisualsTests/visuals/Timeline/timelineTests.ts
@@ -0,0 +1,35 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("Timeline", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("svg.timeline g.cursorsArea > *")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.Timeline();
+        clientHelpers.clickElement(visual.cellRects.eq(0));
+
+        var firstCell = visual.cellRects.eq(0);
+        var selectionColor = firstCell.css('fill');
+
+        visual.cellRects.toArray().map($).forEach(e => {
+            clientHelpers.assertColorsMatch(e.css('fill'), selectionColor, !e.is(firstCell));
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("$46.22K");
+            done();
+        }, 500);
+    });
+}));
