023872605bb17b4d3acf4ebcb48909f7b89905a3
Denis Kudelin <v-dekude@microsoft.com>
2016-08-29 15:00:14 +0300

    Added test for ChicletSlicer

---
+++ src/CustomVisualsTests/visuals/ChicletSlicer/chicletSlicerTest.ts
@@ -0,0 +1,35 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("ChicletSlicer", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+    
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("div.chicletSlicer div.cell > *")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.ChicletSlicer();
+        var unselectedColor = visual.slicerItemContainers.eq(0).css('background-color');
+
+        clientHelpers.clickElement(visual.slicerItemContainers.eq(0), true);
+        clientHelpers.clickElement(visual.slicerItemContainers.eq(1), true);
+
+        visual.slicerItemContainers.toArray().map($).forEach((e,i) => {
+            clientHelpers.assertColorsMatch(e.css('background-color'), unselectedColor, i < 2);
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("$159M");
+            done();
+        }, 500);
+    });
+}));
---
+++ src/CustomVisualsTests/visuals/ChicletSlicer/helpers.ts
@@ -0,0 +1,60 @@
+﻿namespace clientVisuals {
+	export class ChicletSlicer {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("div.chicletSlicer").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement(): JQuery {
+            return this.rootElement.children("div.chicletSlicer");
+        }
+
+        public get slicerBody(): JQuery {
+            return this.mainElement.children("div.slicerBody");
+        }
+
+        public get searchHeader(): JQuery {
+            return this.mainElement.children("div.searchHeader");
+        }
+
+        public get slicerHeader(): JQuery {
+            return this.mainElement.children("div.slicerHeader");
+        }
+
+        public get slicerHeaderText(): JQuery {
+            return this.slicerHeader.children("div.headerText");
+        }
+
+        public get visibleGroup(): JQuery {
+            return this.mainElement
+                .children("div.slicerBody")
+                .children("div.scrollRegion")
+                .children("div.visibleGroup");
+        }
+
+        public get visibleGroupRows() {
+            return this.visibleGroup.children("div.row");
+        }
+
+        public get visibleGroupCells() {
+            return this.visibleGroupRows.children("div.cell");
+        }
+
+        public get slicerTextElements(): JQuery {
+            return this.visibleGroup.find(".slicerText");
+        }
+
+        public get slicerItemContainers(): JQuery {
+            return this.visibleGroupCells.children("ul").children(".slicerItemContainer");
+        }
+
+        public get slicerItemImages(): JQuery {
+            return this.slicerItemContainers.children("img.slicer-img-wrapper");
+        }
+	}
+}
