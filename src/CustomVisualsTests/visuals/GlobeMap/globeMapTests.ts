5055729488f5625c9fde7c968742eb246674ad6a
Denis Kudelin <v-dekude@microsoft.com>
2016-08-30 16:49:35 +0300

    Added test for GlobeMap

---
+++ src/CustomVisualsTests/visuals/GlobeMap/globeMapTests.ts
@@ -0,0 +1,44 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("GlobeMap", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("div > div + div > canvas")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("dom validation tests", function (done) {
+        var visual = new clientVisuals.GlobeMap();
+        setTimeout(() => {
+            var gl = visual.getCanvasContext();
+
+            expect(gl.checkFramebufferStatus(gl.FRAMEBUFFER)).toBe(gl.FRAMEBUFFER_COMPLETE);
+            expect(gl.drawingBufferWidth).toBeGreaterThan(100);
+            expect(gl.drawingBufferHeight).toBeGreaterThan(100);
+
+            var size = {width: 50, height: 50 };
+            var pixels = new Uint8Array(size.width * size.height * 4);
+            gl.readPixels(
+                gl.drawingBufferWidth/2,
+                gl.drawingBufferHeight/2,
+                size.width,
+                size.height,
+                gl.RGBA,
+                gl.UNSIGNED_BYTE,
+                pixels);
+
+            expect(_.uniq(pixels).length).toBeGreaterThan(5);
+
+            done();
+        }, 5000);
+    });
+}));
---
+++ src/CustomVisualsTests/visuals/GlobeMap/helpers.ts
@@ -0,0 +1,28 @@
+﻿namespace clientVisuals {
+	export class GlobeMap {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("div > div + div > canvas").parent().parent().parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement() {
+            return this.rootElement.children("div");
+        }
+
+        public get canvas() {
+            return this.mainElement
+                .children("div")
+                .children("canvas");
+        }
+
+        public getCanvasContext(): WebGLRenderingContext {
+            var canvas = <HTMLCanvasElement>this.canvas.get(0);
+            return <any>canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+        }
+	}
+}
