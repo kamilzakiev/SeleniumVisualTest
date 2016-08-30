9d78c991d5cd831e9c97296bb4110fc0bb9d40a6
Denis Kudelin <v-dekude@microsoft.com>
2016-08-30 13:24:45 +0300

    Added test for StreamGraph

---
+++ src/CustomVisualsTests/visuals/StreamGraph/helpers.ts
@@ -0,0 +1,75 @@
+﻿namespace clientVisuals {
+	export class StreamGraph {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.streamGraph").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement(): JQuery {
+            return this.rootElement.children("svg.streamGraph");
+        }
+
+        public get axisGraphicsContext(): JQuery {
+            return this.mainElement.children("g.axisGraphicsContext");
+        }
+
+        public get xAxisTicks(): JQuery {
+            return this.axisGraphicsContext.children("g.x.axis").children("g.tick");
+        }
+
+        public get yAxisTicks(): JQuery {
+            return this.axisGraphicsContext.children("g.y.axis").children("g.tick");
+        }
+
+        public get xAxisLabel(): JQuery {
+            return this.axisGraphicsContext.children("text.xAxisLabel");
+        }
+
+        public get yAxisLabel(): JQuery {
+            return this.axisGraphicsContext.children("text.yAxisLabel");
+        }
+
+        public get dataLabelsText(): JQuery {
+            return this.mainElement
+                .children("g.labels")
+                .children("text.data-labels");
+        }
+
+        public get layers(): JQuery {
+            return this.mainElement
+                .children("g.dataPointsContainer")
+                .children("path.layer");
+        }
+
+        public get legendGroup(): JQuery {
+            return this.rootElement
+                .children("svg.legend")
+                .children("g#legendGroup");
+        }
+
+        public get legendOrientation(): string {
+            return this.rootElement
+                .children("svg.legend")
+                .attr("orientation");
+        }
+
+        public get legendWidth(): number {
+            return this.rootElement
+                .children("svg.legend")
+                .width();
+        }
+
+        public get legendTitle(): JQuery {
+            return this.legendGroup.children(".legendTitle");
+        }
+
+        public get legendItemText(): JQuery {
+            return this.legendGroup.children(".legendItem").children("text.legendText");
+        }
+	}
+}
---
+++ src/CustomVisualsTests/visuals/StreamGraph/streamGraphTests.ts
@@ -0,0 +1,37 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("StreamGraph", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("svg.streamGraph g.dataPointsContainer > *")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.StreamGraph();
+        clientHelpers.clickElement(visual.layers.eq(0));
+        clientHelpers.clickElement(visual.layers.eq(1), true);
+
+        visual.layers.toArray().map($).forEach((e,i) => {
+            if(i >= 2) {
+                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+            } else {
+                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+            }
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("191.60");
+            done();
+        }, 500);
+    });
+}));
