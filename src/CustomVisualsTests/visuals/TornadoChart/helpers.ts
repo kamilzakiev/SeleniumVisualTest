3584b2a3f0f75dd6e8a68dae991afb3f1d6d9161
Denis Kudelin <v-dekude@microsoft.com>
2016-08-30 13:15:43 +0300

    Added test for TornadoChart

---
+++ src/CustomVisualsTests/visuals/TornadoChart/helpers.ts
@@ -0,0 +1,45 @@
+﻿namespace clientVisuals {
+	export class TornadoChart {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.tornado-chart").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement() {
+            return this.rootElement.children("svg.tornado-chart");
+        }
+
+        public get scrollable() {
+            return this.rootElement.children("svg.tornado-chart").children("g");
+        }
+
+        public get scrollbar() {
+            return this.mainElement.children("g.y.brush");
+        }
+
+        public get scrollbarRect() {
+            return this.scrollbar.children("rect.extent");
+        }
+
+        public get categories() {
+            return this.scrollable.children("g.categories").children("g.category");
+        }
+
+        public get axis() {
+            return this.scrollable.children("g.axes").children("line.axis");
+        }
+
+        public get columns() {
+            return this.scrollable.children("g.columns").children("rect.column");
+        }
+
+        public get labels() {
+            return this.scrollable.children("g.labels").children("g.label");
+        }
+	}
+}
---
+++ src/CustomVisualsTests/visuals/TornadoChart/tornadoChartTests.ts
@@ -0,0 +1,37 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("TornadoChart", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("svg.tornado-chart g.columns > *")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.TornadoChart();
+        clientHelpers.clickElement(visual.columns.eq(0));
+        clientHelpers.clickElement(visual.columns.eq(1), true);
+
+        visual.columns.toArray().map($).forEach((e,i) => {
+            if(i % (visual.columns.length/2) >= 2) {
+                expect(parseFloat(e.css('fill-opacity'))).toBeLessThan(1);
+            } else {
+                expect(parseFloat(e.css('fill-opacity'))).toBe(1);
+            }
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("82.40K");
+            done();
+        }, 500);
+    });
+}));
