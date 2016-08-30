f7647f73adf6cf95ab77f269c0dc8f64d0e968c2
Denis Kudelin <v-dekude@microsoft.com>
2016-08-30 14:47:35 +0300

    Added test for LineDotChart

---
+++ src/CustomVisualsTests/visuals/LineDotChart/helpers.ts
@@ -0,0 +1,28 @@
+﻿namespace clientVisuals {
+	export class LineDotChart {
+        private rootElementValue: JQuery;
+
+		constructor(element?: JQuery) {
+            this.rootElementValue = (element || clientHelpers.getVisualsRootElements()).find("svg.lineDotChart").parent();
+		}
+
+		public get rootElement() {
+			return this.rootElementValue;
+		}
+
+        public get mainElement(): JQuery {
+            return this.rootElement.children("svg.lineDotChart");
+        }
+
+        public get line() {
+            return this.mainElement
+                .children("g")
+                .children("g.line");
+        }
+
+        public get dots() {
+            return this.line
+                .children("circle.point");
+        }
+	}
+}
---
+++ src/CustomVisualsTests/visuals/LineDotChart/lineDotChartTests.ts
@@ -0,0 +1,36 @@
+import {WebdriverIO, webdriverIOHelpers, config, webdriverIOClientModule} from "../../_references";
+
+describe("LineDotChart", config.getSpecs(__dirname, (browser, reportUrl) => {
+    let client: WebdriverIO.Client<void>;
+    let clientModule: webdriverIOClientModule = new webdriverIOClientModule([__dirname + "/helpers.js"], function() {
+    });
+    let itClient = clientModule.getItClient(() => client), xitClient = clientModule.getXitClient(() => client);
+
+    beforeEach((done) => {
+        client = webdriverIOHelpers.getWebClient(browser);
+        client
+            .url(reportUrl)
+            .waitForVisible("svg.lineDotChart g.line > circle.point")
+            .then(() => done());
+    });
+
+    afterEach((done) => client.endAll().finally(() => done()));
+
+    itClient("selection test", function (done) {
+        var visual = new clientVisuals.LineDotChart();
+        clientHelpers.clickElement(visual.dots.eq(0));
+
+        visual.dots.toArray().map($).forEach((e,i) => {
+            if(i >= 1) {
+                expect(e).not.toHaveClass("point_selected");
+            } else {
+                expect(e).toHaveClass("point_selected");
+            }
+        });
+
+        setTimeout(() => {
+            expect(clientHelpers.getTextWithoutChild($("svg.card > g > text.value"))).toBe("1");
+            done();
+        }, 500);
+    });
+}));
